c1bad0edefc9f1064072db066293ef3f2c2c49f6
Sam Huynh <sam@hungryhungry.com>
2021-11-12 14:16:42 +1100

    Add http to schema registry url

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 
-export const host = "localhost:8081";
+export const host = "http://localhost:8081";
 
 const schemaFile = 'sam.avsc';
 export const absolutePathToSchema = path.join(__dirname, schemaFile);
